b5c4b6a96d659ebddb745921fa6500a4c32f1504
Ravikiran <ravi@DESKTOP-8AJ9JMC.localdomain>
2025-07-13 20:12:04 +0530

    mess

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App';
 import App from './App.jsx';
 import './index.css';
 
